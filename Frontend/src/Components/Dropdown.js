3c13b87e943002b582dd3d5123fbc991fa7e8f81
NikoletaPantelidou <nikolpant2@gmail.com>
2023-07-25 14:05:22 +0300

    comments on css, dropdown edit

--- Frontend/src/Components/Dropdown.js
+++ Frontend/src/Components/Dropdown.js
@@ -4,9 +4,11 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 function DropdownBar() {
   return (
     <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      <Dropdown.Item href="#/action-1">XS</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">S</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">M</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">L</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">XL</Dropdown.Item>
     </DropdownButton>
   );
 }
