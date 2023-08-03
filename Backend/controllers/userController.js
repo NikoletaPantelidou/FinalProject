2a7869638033d3db632255cb52cd3aede4d84eef
NikoletaPantelidou <nikolpant2@gmail.com>
2023-08-03 13:38:34 +0300

    auth changes 2

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -33,7 +33,10 @@ var userLogin = async (req, res) => {
       if (result) {
         //create the token to send it back to the front
         //payload + secret ==> token
-        var token = jwt.sign({ id: user._id }, "greenfield");
+        var token = jwt.sign({ id: user._id }, "123", process.env.TOKEN_KEY, {
+          expiresIn: "2h",
+        });
+        user.token = token;
         res.send({ token });
       } else {
         res.status(500).json({ error: "Wrong password" });
@@ -43,6 +46,21 @@ var userLogin = async (req, res) => {
     res.status(500).json({ error: "Wrong username" });
   }
 };
+
+/*const getUserInfoByToken = async (req, res, next) => {
+  const token= user.token
+  const id = req.params.id;
+  let user;
+  try {
+    user = await Book.findById(id);
+  } catch (error) {
+    console.log(error);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "No Book found" });
+  }
+  return res.status(200).json({ book });
+}; */
 
 module.exports = {
   userLogin,
